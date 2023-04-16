6c0fcc8122bca97c272852801e7a23d03c4677b5
Dirck Hecking <justadirck@gmail.com>
2023-04-16 02:35:32 -0400

    landing page

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="header">
+  <img class="logo" src="/svelte.svg" alt="logo" />
+</div>
